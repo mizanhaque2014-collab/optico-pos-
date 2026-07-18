c754481cb1441e893e66d1867ce8381551f95da4
mizanhaque2014-collab <mizanhaque2014@gmail.com>
2026-07-18 12:19:48 -0700

    feat: add script to create new prescriptions
    Introduce a helper function to send prescription data to the Google Apps Script endpoint for database synchronization.

---
+++ get_trace.js
@@ -0,0 +1,19 @@
+async function run() {
+  const url = 'https://script.google.com/macros/s/AKfycbw279jQdZtQPJEzDFxb1oC59Xn6OALuo6f8Z_I5IGaCsOz5Vu_vuP0HZis1yghgVeRc-g/exec';
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain;charset=utf-8' },
+    body: JSON.stringify({
+      action: 'createPrescription',
+      prescription: {
+        CustomerID: 'CUST-1784376019371449',
+        CompanyID: 'COMP-1',
+        BranchID: 'BR-1',
+        ExamDate: '2024-01-01'
+      }
+    })
+  });
+  const data = await res.text();
+  console.log("RESPONSE:", data);
+}
+run();
